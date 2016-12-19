16a2efd9a6842439e9e8c35de955b392d4572de9
FengZStrong <fengz_strong@126.com>
2016-12-19 18:50:20 +0800

    modify HasPtr.h

--- c++primer/unit13/13.1.3/HasPtr.h
+++ c++primer/unit13/13.1.3/HasPtr.h
@@ -7,9 +7,12 @@ public:
 	HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0) {}
 	HasPtr(const HasPtr &hp) : ps(new std::string(*(hp.ps))), i(hp.i) { std::cout << "copy constructor func" << std::endl; }
 	HasPtr& operator=(const HasPtr &hp) {
-		*ps = *(hp.ps);
+		auto newp = new std::string(*hp.ps);
+		delete ps;
+		ps = newp;
 		i = hp.i;
 		std::cout << "operator=" << std::endl;
+		return *this;
 	}
 
 	~HasPtr() {
