bea605e3cddee0a04505760901955682e3465983
FengZStrong <fengz_strong@126.com>
2016-12-19 15:40:18 +0800

    modify 13.1.1

--- c++primer/unit13/13.1.1/HasPtr.h
+++ c++primer/unit13/13.1.1/HasPtr.h
@@ -1,10 +1,11 @@
 #include <string>
+#include <iostream>
 
 class HasPtr
 {
 public:
 	HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0) {}
-	HasPtr(const HasPtr &hp) : ps(hp.ps), i(hp.i) {}
+	HasPtr(const HasPtr &hp) : ps(hp.ps), i(hp.i) { std::cout << "copy constructor func" << std::endl; }
 	std::string *ps;
 private:
 	int i;
--- c++primer/unit13/13.1.1/main.cc
+++ c++primer/unit13/13.1.1/main.cc
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "HasPtr.h"
 
 int main()
