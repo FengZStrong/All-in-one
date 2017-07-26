7884396c985bc6eabd2a996b869d2c09aabb1ee8
FengZStrong <fengz_strong@126.com>
2017-07-26 11:24:50 +0800

    modify 13.1.1

--- c++primer/unit13/13.1.1/HasPtr.h
+++ c++primer/unit13/13.1.1/HasPtr.h
@@ -5,8 +5,16 @@ class HasPtr
 {
 public:
 	HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0) {}
-	HasPtr(const HasPtr &hp) : ps(hp.ps), i(hp.i) { std::cout << "copy constructor func" << std::endl; }
-	std::string *ps;
+	HasPtr(const HasPtr &hp) : ps(new std::string(*hp.ps)), i(hp.i) 
+    { 
+        std::cout << "copy constructor func" << std::endl; 
+    }
+    ~HasPtr()
+    {
+        delete ps;
+    }
+public:
+    std::string *ps;
 private:
 	int i;
 };
--- c++primer/unit13/13.1.1/main.cc
+++ c++primer/unit13/13.1.1/main.cc
@@ -3,8 +3,14 @@
 int main()
 {
 	std::string hw("hello world");
-	HasPtr h(hw);
-	HasPtr h1 = h;
-	std::cout << *h1.ps << std::endl;
-	delete h1.ps;
+	HasPtr *h = new HasPtr(hw);
+	HasPtr h1(*h);
+    delete h;
+	std::cout << *(h1.ps) << std::endl;
+	//delete h1.ps;
+
+	std::string *s = new std::string("fuck");
+	std::string s1(*s);
+	delete s;
+	std::cout << s1 << std::endl;
 }
